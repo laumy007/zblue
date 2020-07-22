1e0f5dd3314570cc15ffc102b75638deaa003c63
chao.an <anchao@xiaomi.com>
2020-07-22 20:30:56 +0800

    Revert "Bluetooth: host: Fix resume advertiser race condition"
    This reverts commit 52f9c0dd465f0b90243ceb390710b7b1bbea0f21.
    
    Change-Id: If52cac9fc8efc991cb4556a498ddd9f9bed5fe49
    Signed-off-by: chao.an <anchao@xiaomi.com>

--- subsys/bluetooth/host/hci_core.c
+++ subsys/bluetooth/host/hci_core.c
@@ -1672,7 +1672,7 @@ static void hci_disconn_complete(struct net_buf *buf)
 	conn = bt_conn_lookup_handle(handle);
 	if (!conn) {
 		BT_ERR("Unable to look up conn with handle %u", handle);
-		return;
+		goto advertise;
 	}
 
 	conn->err = evt->reason;
@@ -1707,6 +1707,11 @@ static void hci_disconn_complete(struct net_buf *buf)
 #endif /* defined(CONFIG_BT_CENTRAL) && !defined(CONFIG_BT_WHITELIST) */
 
 	bt_conn_unref(conn);
+
+advertise:
+	if (IS_ENABLED(CONFIG_BT_PERIPHERAL)) {
+		bt_le_adv_resume();
+	}
 }
 
 static int hci_le_read_remote_features(struct bt_conn *conn)
